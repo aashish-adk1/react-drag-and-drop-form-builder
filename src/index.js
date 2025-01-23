ae92311a0f36fadc7abf1fdddbb19a49ee449a92
aashish-adk1 <aashish.adk156@gmail.com>
2025-01-23 14:12:45 +0545

    built

--- src/index.js
+++ src/index.js
@@ -21,24 +21,6 @@ const formData = [
   }
 ];
 
-/* 
-The order of the imports and requires is very important, especially in the online enviornment.
-The two jQuery libraries must be imported using Node's require(), and not ES6 import.
-Also, these two requires MUST come after setting the global jQuery and $ symbols.
-
-In my Babel/Webpack project, the type and order of the imports is a little less sensitive.
-For my project, the following alternative works:
-
-    import $ from 'jquery';
-    import React from 'react';
-    import ReactDOM from 'react-dom';
-    import 'jquery-ui-sortable';
-
-    window.jQuery = $;
-    window.$ = $;
-
-    require('formBuilder');
-*/
 
 class FormBuilder extends Component {
   fb = createRef();
